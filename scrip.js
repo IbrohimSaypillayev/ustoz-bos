03c298fe6c10b6612679f4795bdbb3b81bd5d6be
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-03-28 16:33:12 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,14 @@
+
+
+
+let arr = ['anor', 'shaftoli' , 'gilos' , 'xurmo']
+
+
+let tasdiq = confirm('bitta array ob tashamoqchimisiz')
+
+
+
+if (tasdiq == true) {
+    arr.pop();
+}
+console.log(arr)
